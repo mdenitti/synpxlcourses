2027c87700aea8e51251964812b374af1ce306e2
mdenitti <massimodn77@gmail.com>
2023-04-24 11:44:32 +0200

    Added one2one relations in models Badge and Teacher

--- app/Models/Badge.php
+++ app/Models/Badge.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Teacher;
 
 class Badge extends Model
 {
     use HasFactory;
+
+    public function teacher()
+    {
+        return $this->hasOne(Teacher::class);
+    }
 }
--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Badge;
 
 class Teacher extends Model
 {
     use HasFactory;
+
+    public function badge()
+    {
+        return $this->belongsTo(Badge::class);
+    }
 }
