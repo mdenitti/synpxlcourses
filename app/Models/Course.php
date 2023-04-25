25e6123ec805374c8772968ab55762bd4d36df7c
mdenitti <massimodn77@gmail.com>
2023-04-25 11:07:56 +0200

    Many2Many relationship setup for courses and student - auto use of the pivot table!

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Student;
 
 class Course extends Model
 {
     use HasFactory;
+  
+    public function students() {
+        return $this->belongsToMany(Student::class);
+    }
 }
--- app/Models/Student.php
+++ app/Models/Student.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Course;
 
 class Student extends Model
 {
     use HasFactory;
+
+    // belongstomany relation with course
+    public function courses()
+    {
+        return $this->belongsToMany(Course::class);
+    }
 }
